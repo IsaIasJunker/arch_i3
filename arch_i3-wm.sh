a4f3ad8e64b2357c268d19e24ee17bc307d96528
IsaIasJunker <isaiasjunker7@gmail.com>
2025-04-10 21:17:31 -0300

    más ajustes

--- arch_i3-wm.sh
+++ arch_i3-wm.sh
@@ -10,34 +10,9 @@ alacritty
 nano
 )
 
-aplicaciones=(
-firefox
-gedit
-nwg-look
-nitrogen
-thunar
-ranger
-htop
-fastfetch
-libreoffice-still    
-)
-
-actualizar(){
-    sudo pacman -S --noconfirm
-}
-
-
 cambiar_layaout(){
     sudo localectl set-keymap la-latin1
     sudo localectl set-x11-keymap latam
-}
-
-instalar_yay (){
-    sudo pacman -Syu --noconfirm
-    sudo pacman -S --noconfirm --needed base-devel git
-    git clone https://aur.archlinux.org/yay.git
-    cd ./yay 
-    makepkg -si
 }
 
 instalar_i3(){
@@ -48,18 +23,9 @@ instalar_aplicaciones(){
     sudo pacman -S --noconfirm "${aplicaciones[@]}"
 }
 
-instalar_sddm(){
-    sudo pacman -S --noconfirm sddm
-
-    sudo systemctl enable sddm.service
-}
-    
 main(){
-    actualizar
     cambiar_layaout
-    instalar_yay
     instalar_i3
-    instalar_sddm
 }
 
 main
