ecb6fca70fd305b592df12bc0268e35d56b4b12b
IsaIasJunker <isaiasjunker7@gmail.com>
2025-04-11 14:03:03 -0300

    Más cambios

--- arch_i3-wm.sh
+++ arch_i3-wm.sh
@@ -8,28 +8,43 @@ i3-wm
 i3status
 dmenu
 alacritty
+thunar
+nwg-look
 network-manager-applet
+fastfetch
+htop
 )
 
+# Cambio el layout del teclado a latam
 cambiar_layaout(){
     sudo localectl set-keymap la-latin1
     sudo localectl set-x11-keymap latam
 }
-
+# Instalo lightdm y lo habilito
 instalar_display_manager(){
     sudo pacman -S lightdm-gtk-greeter
     sudo systemctl enable lightdm.service
 }
 
+# Instalo el arreglo de paquetes que tengo definido
 instalar_i3(){
     sudo pacman -S --noconfirm "${componentes_i3[@]}"
 }
 
+# Instalo el helper yay
+instalar_yay(){
+    sudo pacman -S --noconfirm --needed base-devel
+    git -C $HOME clone https://aur.archlinux.org/yay.git
+    cd $HOME/yay/
+    makepkg -si 
+}
 
+# Ejecuto de forma secuencial las funciones
 main(){
     cambiar_layaout
     instalar_i3
     instalar_display_manager
+    instalar_yay
 }
 
 main
