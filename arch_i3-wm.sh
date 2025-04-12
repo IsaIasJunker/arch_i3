1b5b93f640a991e1c3c0d26c952b872498dd204e
IsaIasJunker <isaiasjunker7@gmail.com>
2025-04-12 13:36:24 -0300

    +Polybar + Rofi

--- arch_i3-wm.sh
+++ arch_i3-wm.sh
@@ -6,7 +6,6 @@ componentes_i3=(
 nano
 i3-wm
 i3status
-dmenu
 alacritty
 thunar
 nwg-look
@@ -14,6 +13,9 @@ network-manager-applet
 fastfetch
 htop
 bash-completion
+rofi
+polybar
+ttf-meslo-nerd
 )
 
 # Cambio el layout del teclado a latam
@@ -21,7 +23,7 @@ cambiar_layaout(){
     sudo localectl set-keymap la-latin1
     sudo localectl set-x11-keymap latam
 }
-# Instalo lightdm y lo habilito
+# Instalo lightmdm y habilito el servicio
 instalar_display_manager(){
     sudo pacman -S --noconfirm lightdm-gtk-greeter
     sudo systemctl enable lightdm.service
@@ -32,9 +34,22 @@ instalar_i3(){
     sudo pacman -S --noconfirm "${componentes_i3[@]}"
 }
 
-copiar_configuracion(){
+
+copiar_i3_config(){
     mkdir -p ~/.config/i3
-    cp -f ./configs/config ~/.config/i3/config
+    cp -f ./i3/config ~/.config/i3/config
+}
+
+configurar_rofi(){
+    mkdir -p ~/.config/rofi
+    mv ./rofi/rofi/config.rasi ~/.config/rofi/
+    mv ./rofi/rofi/catppuccin-mocha.rasi  ~/.config/rofi/
+}
+
+configurar_polybar(){
+    mkdir -p ~/.config/polybar
+    mv ./polybar/config.ini ~/.config/polybar/
+    mv ./polybar/launch_polybar.sh ~/.config/polybar/
 }
 
 # Ejecuto de forma secuencial las funciones
@@ -42,6 +57,8 @@ main(){
     cambiar_layaout
     instalar_i3
     instalar_display_manager
-    copiar_configuracion
+    copiar_i3_config
+    configurar_polybar
+    configurar_rofi
 }
 main
